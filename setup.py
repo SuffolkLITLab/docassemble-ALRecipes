1eb0c6f97e02ec7822479994c2c5540871828dab
lily yang <20231649+purplesky2016@users.noreply.github.com>
2022-02-24 10:53:05 -0500

    Add dependency

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(name='docassemble.ALRecipes',
       url='https://docassemble.org',
       packages=find_packages(),
       namespace_packages=['docassemble'],
-      install_requires=[],
+      install_requires=['mechanize>=0.4.7'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/ALRecipes/', package='docassemble.ALRecipes'),
      )
