93ac7f2391a00e75bbc365a3f8b28545fd913626
Bryce Willey <bryce.willey@suffolk.edu>
2025-11-24 16:58:53 -0500

    Use implicit namespaces over explicit namespaces

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 from fnmatch import fnmatchcase
 from distutils.util import convert_path
 
@@ -51,8 +51,7 @@ setup(name='docassemble.ALRecipes',
       author_email='52798256+plocket@users.noreply.github.com',
       license='The MIT License (MIT)',
       url='https://docassemble.org',
-      packages=find_packages(),
-      namespace_packages=['docassemble'],
+      packages=find_namespace_packages(),
       install_requires=['mechanize>=0.4.5'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/ALRecipes/', package='docassemble.ALRecipes'),
